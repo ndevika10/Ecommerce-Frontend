f0136bb97b7101aab177b039faceb51cae35893b
ndevika10 <ndevika2003@gmail.com>
2025-09-11 23:25:33 +0530

    url changed

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://ecommerce-backend-production-f86e.up.railway.app"
+    ? "ecommerce-backend-production-ce7d.up.railway.app"
     : "http://localhost:8080";
 
 const api = axios.create({
